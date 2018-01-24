59fc4fb8b195d58154abd8f00b7641cbdc97f16d
nonop1507 <nono1507@naver.com>
2018-01-24 14:50:55 +0900

    post

--- msite/urls.py
+++ msite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from django.conf.urls import include, url
 from django.contrib import admin
 
-urlpatterns = {
-    path('admin/', admin.site.urls),
+urlpatterns = [
+    url('admin/', admin.site.urls),
     url(r'', include('blog.urls')),
-}
+]
